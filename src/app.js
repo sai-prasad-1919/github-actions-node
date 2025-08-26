6b9459bc169ebdd5f8fcfe04d0e5700fb6c3f2f6
sai-prasad-1919 <psai6269@gmail.com>
2025-08-26 17:57:38 +0530

    chages the message

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/testNode", (_req,res) => {
-    res.status(200).send("testNode endpoint is working")
+    res.status(200).send("testNode endpoint is working fine")
 })
 
 module.exports = app;
